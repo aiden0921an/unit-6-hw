dfbf70c5cd3284d47cfeb75a52d5052949f39a78
aiden0921an <nelsonaiden23@gmail.com>
2024-09-05 21:31:03 -0500

    started JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,22 @@
 
 
-
+//grabs search history from local storage otherwise returns an empty array
+const cityHistory= document.getElementById('cities')
 const searchHistory=JSON.parse(localStorage.getItem('searchHistory')) || [];
 
 
 
+function searchHistory(){
+    if (!cityHistory.includes(city)){
+        cityHistory.push(city)
+        localStorage.setItem('searchHistory', JSON.stringify(searchHistory))
+        //add function to render
+    }
+}
+
+
+function renderSearchedCity(){
+    searchHistory.forEach((city) => {
+        
+    })
+}
